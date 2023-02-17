5e72e3e01b64539d0305564328cf793ef29a09ac
Joakim L. Christiansen <{ID}+{username}@users.noreply.github.com>
2023-02-17 13:07:48 +0100

    .

--- useful.js
+++ useful.js
@@ -1,17 +1,17 @@
 
 export function initLightSwitch(stylesheetId, buttonId) {
-  const stylesheet_ = document.getElementById(stylesheetId)
+  const stylesheet = document.getElementById(stylesheetId)
   const lightSwitch = document.getElementById(buttonId)
   function colorSchemeChange(dark) {
     if (typeof dark == 'string') dark = dark == 'true'
-    stylesheet_.disabled = dark
+    stylesheet.disabled = dark
     lightSwitch.innerText = dark ? 'Lights on' : 'Lights off'
     localStorage.setItem('dark', dark)
   }
   const mql = window.matchMedia('(prefers-color-scheme: dark)')
   mql.addEventListener('change', e => colorSchemeChange(e.matches))
   colorSchemeChange(localStorage.getItem('dark') ?? mql.matches)
-  lightSwitch.addEventListener('click', () => colorSchemeChange(!stylesheetId.disabled))
+  lightSwitch.addEventListener('click', () => colorSchemeChange(!stylesheet.disabled))
 }
 
 export async function registerServiceWorker(url, options, installButtonId) {
